0ea07e711499f7510af9e6fba316535eca80a4d2
ksdyb.park <ksdyb@amazon.com>
2024-03-08 09:08:09 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1234,6 +1234,7 @@ def getResponse(connectionId, jsonBody):
     reference = ""
     isControlMsg = False
     token_counter_input = 0
+    time_for_revise = 0
     
     if type == 'text' and body[:11] == 'list models':
         isControlMsg = True
