ee7b9f67adf542652acc41f4634b12788ae97105
ksdyb.park <ksdyb@amazon.com>
2024-03-09 09:06:56 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -234,7 +234,6 @@ for (i=0;i<maxMsgItems;i++) {
 calleeName.textContent = "Chatbot";  
 calleeId.textContent = "AWS";
 
-
 if(langstate=='korean') {
     addNotifyMessage("Amazon Bedrock을 이용하여 채팅을 시작합니다.");
     addReceivedMessage(uuidv4(), "Amazon Bedrock을 이용하여 주셔서 감사합니다. 편안한 대화를 즐기실수 있으며, 파일을 업로드하면 요약을 할 수 있습니다.")
