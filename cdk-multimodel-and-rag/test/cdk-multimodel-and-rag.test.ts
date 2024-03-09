7148047fa55749b53ec75bfaa1a9ad0eb9f8eec7
ksdyb.park <ksdyb@amazon.com>
2024-03-09 10:28:14 +0900

    update

---
+++ cdk-multimodel-and-rag/test/cdk-multimodel-and-rag.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMultimodelAndRag from '../lib/cdk-multimodel-and-rag-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-multimodel-and-rag-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMultimodelAndRag.CdkMultimodelAndRagStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
