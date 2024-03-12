4483bcacc9f95a3e859a410e6939c262d8ebd9d6
ksdyb.park <ksdyb@amazon.com>
2024-03-13 08:46:53 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -341,7 +341,10 @@ def check_supported_type(key, file_type, size):
         print('html: ', key.find('/html/'))
         return False
     
-    if size > 5000 and size<max_object_size and isSupported(file_type):
+    if key[0]=='.' or key[key.rfind('/')+1]=='.':
+        print(f"Ignore {key} since the filename starts a dot character for macbook.")        
+        return False
+    elif size > 5000 and size<max_object_size and isSupported(file_type):
         return True
     
     if size > 0 and (file_type == 'txt' or file_type == 'md' or file_type == 'py' or file_type == 'js'):
