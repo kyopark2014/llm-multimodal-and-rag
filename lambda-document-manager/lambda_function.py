10cabdd476febbb31e98f843dd2eca3c84e4da1e
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-25 07:50:07 +0900

    update

--- lambda-document-manager/lambda_function.py
+++ lambda-document-manager/lambda_function.py
@@ -300,7 +300,10 @@ def store_image_for_opensearch(key):
                         
     width, height = img.size 
     print(f"width: {width}, height: {height}, size: {width*height}")
-                        
+            
+    if width < 100 or height < 100:  # skip small size image
+        return []
+                
     isResized = False
     while(width*height > 5242880):
         width = int(width/2)
